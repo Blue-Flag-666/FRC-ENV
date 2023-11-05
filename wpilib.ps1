b08f0f6a764535ed9605111b14dea0bf570154cc
Blue-Flag-666 <64064866+Blue-Flag-666@users.noreply.github.com>
2023-11-05 13:56:42 +0800

    Update wpilib.ps1

--- wpilib.ps1
+++ wpilib.ps1
@@ -1,7 +1,8 @@
-$downloadUri= "https://packages.wpilib.workers.dev/installer/v2024.1.1-beta-3/Win64/WPILib_Windows-2024.1.1-beta-3.iso"
-
-Invoke-WebRequest -Uri $downloadUri -Out "wpilib.iso"
 $iso = Mount-DiskImage -PassThru -ImagePath C:\projects\frc-env\wpilib.iso
 $volume = Get-Volume -DiskImage $iso
 $path =  $volume.DriveLetter + ":\WPILibInstaller.exe"
-iex $path
+
+Start-Process $path -Wait
+
+Dismount-DiskImage -DevicePath $iso.devicePath | Out-Null
+Remove-Item .\wpilib.iso
