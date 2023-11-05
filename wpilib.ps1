9d08f767876220c659bfb3ba44c86f7198ab755f
Blue-Flag-666 <64064866+Blue-Flag-666@users.noreply.github.com>
2023-11-05 14:07:51 +0800

    Update wpilib.ps1

--- wpilib.ps1
+++ wpilib.ps1
@@ -5,6 +5,7 @@ $iso = Mount-DiskImage -PassThru -ImagePath C:\projects\frc-env\wpilib.iso
 $volume = Get-Volume -DiskImage $iso
 $path =  $volume.DriveLetter + ":\WPILibInstaller.exe"
 
+Write-Warning "Build paused. Open a RDP session to install WPILib."
 Start-Process $path -Wait
 
 Dismount-DiskImage -DevicePath $iso.devicePath | Out-Null
